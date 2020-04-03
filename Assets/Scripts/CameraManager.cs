0030f427cdb10293dc38a9a3dcedd7b6f9a1286b
Callum <Calxferg@gmail.com>
2020-04-03 18:41:21 -0400

    projector

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Projector projector;
+
+    public Renderer quadRend;
+
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+
+        Material newMaterial = new Material(projector.material);
+        newMaterial.mainTexture = webcamTexture;
+        projector.material = newMaterial;
+
+        quadRend.material.mainTexture = webcamTexture;
+
+        webcamTexture.Play();
+
+        float aspectRatio = (float)webcamTexture.width / (float)webcamTexture.height;
+        float scale = quadRend.gameObject.transform.localScale.x;
+
+        quadRend.gameObject.transform.localScale = new Vector3(aspectRatio * scale, scale, 1);
+    }
+
+    void Update()
+    {
+
+    }
+}
